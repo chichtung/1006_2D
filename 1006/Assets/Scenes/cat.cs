920bed8e0efb58bea8d81755a6ed101ec4460e1b
chichtung <54761445+chichtung@users.noreply.github.com>
2019-11-10 12:19:43 +0800

    吃掉道具設定完成

--- 1006/Assets/Scenes/cat.cs
+++ 1006/Assets/Scenes/cat.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class cat : MonoBehaviour
 {
     #region 欄位區域
-    [Header("跳躍次數")] [Range(1, 10)]
+    [Header("跳躍次數")]
+    [Range(1, 10)]
     public int jumpCount = 2;
-    [Header("跳躍高度")] [Range(10, 1000)]
+    [Header("跳躍高度")]
+    [Range(10, 1000)]
     public int jump = 100;
-    [Header("速度")] [Range(0f, 10f)]
+    [Header("速度")]
+    [Range(0f, 10f)]
     public float speed = 2.5f;
     [Header("是否在地面")]
     public bool isground = false;
@@ -33,14 +37,26 @@ public class cat : MonoBehaviour
     public AudioClip Audiojump; //AudioClop 跳躍聲音
     public AudioClip AudioSliding; ////AudioClop 滑行聲音
     private SpriteRenderer Sr; //修改顏色
-    
+    public Tilemap tileprop;
+    public Text Dpdiamond; //顯示鑽石文字
+    public int Cpdiamond;  //鑽石整數
+    public int CpCherry; //櫻桃整數
+    public Text DpCherry; // 顯示櫻桃文字
+    public float lose = 1;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.name == "地板")
         {
             isground = true;
         }
- 
+        if (collision.collider.name == "櫻桃")
+        {
+            Eatprop(collision);
+            CpCherry++;
+            DpCherry.text = CpCherry + "";
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -49,8 +65,18 @@ public class cat : MonoBehaviour
             Sprite();
 
         }
+        if (collision.tag == "鑽石")
+        {
+            Eatdiamond(collision);
+            Cpdiamond++;
+            Dpdiamond.text = Cpdiamond + "";
+        }
     }
 
+    private void Eatdiamond(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
 
     private void Start()
     {
@@ -84,13 +110,14 @@ public class cat : MonoBehaviour
     {
         MoveCat();
         MoveCom();
+        HpLose();
     }
     /// <summary>
     /// 貓的移動方法
     /// </summary>
     private void MoveCat()
     {
-        Cat.Translate(speed*Time.deltaTime, 0, 0);
+        Cat.Translate(speed * Time.deltaTime, 0, 0);
 
     }
     /// <summary>
@@ -99,7 +126,7 @@ public class cat : MonoBehaviour
     private void MoveCom()
     {
         //欄位.位移(速度*每秒*/1,y,z)
-        com.Translate(speed*Time.deltaTime, 0, 0);
+        com.Translate(speed * Time.deltaTime, 0, 0);
 
     }
     /// <summary>
@@ -134,7 +161,26 @@ public class cat : MonoBehaviour
         cc2D.offset = new Vector2(-0.008f, -0.1f);
         cc2D.size = new Vector2(0.5f, 1.4f);
     }
+    private void Eatprop(Collision2D collision)
+    {
+        Debug.Log("吃到櫻桃了");
+        Vector3 center = Vector3.zero;
+        Vector3 hitpoint = collision.contacts[0].point;
+        Vector3 normal = collision.contacts[0].normal;
+
+        center.x = hitpoint.x - normal.x * 0.01f;
+        center.y = hitpoint.y - normal.y * 0.01f;
+
+        tileprop.SetTile(tileprop.WorldToCell(center), null);
+
+    }
+    public void HpLose()
+    {
+        hp -= Time.deltaTime* lose;
+        ig.fillAmount = hp / maxhp;
+    }
 }
+
 
 
 
